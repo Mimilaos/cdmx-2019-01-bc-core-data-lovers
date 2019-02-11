b785332ff672c99c536e691030a5462e93e1c905
giovanna08 <priscila_glz_27@hotmail.es>
2019-02-10 20:38:38 -0600

    Accede a data, no terminado

--- src/main.js
+++ src/main.js
@@ -1,25 +1,17 @@
-const dataPokemon = POKEMON.pokemon;
 const search = document.getElementById(seeker);
-const idPokemon = document.getElementById(idPokemon);
-const infoPokemon = document.getElementById(info-pokemon);
+const dataPokemon = POKEMON.pokemon;
+// const print = `<textarea name="name" id="name-pokemon">${name}</textarea>`;
 
-
-const print = (num, name, img);
-
-data.forEach(element =>{
-    //let id = element.id;
-    //let num = element.num;
-    //let name = element.name;
-    //let img = element.img;
-    //let type = element.type;
-    //let heigt= elemen.height;
-    //let weight= elemen.weight;
-    //let candy= elemen.candy;
-    //let candyCount= element.candy_count;
-    //let egg= element.egg;
-    //let spawnChance= element.spawn_chance;
-    //let avgSpawns = element.avg_spawns;
-    //let spawnTime = element.spawn_time;
+dataPokemon.forEach(element =>{
+    let name = element.name;
+    let num = element.num;
+    let img = element.img;
+    let type = element.type;
+    let height = element.height;
+    let weight = element.weight;
+    console.log(weight);
 })
-//print.innerAdjacentHTML("beforeend", result)
-return search;
+ 
+
+
+
