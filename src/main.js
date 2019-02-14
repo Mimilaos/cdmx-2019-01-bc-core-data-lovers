11eb75ee992f607edf4882b937f8bcb24247cc70
giovanna08 <priscila_glz_27@hotmail.es>
2019-02-14 14:37:41 -0600

    intento 1, filtrado por nombre

--- src/main.js
+++ src/main.js
@@ -1,42 +1,64 @@
-const search = document.getElementById('seeker');
+
 const selector = document.getElementById('class-selector');
+//const imprimir=document.getElementById('listTypePokemon');
+//const nombrePokemon = document.getElementById('name-pokemon');
 const dataPokemon = POKEMON.pokemon;
-const imprimir=document.getElementById('listTypePokemon');
 
-dataPokemon.forEach(element => {
-    let name = element.name;
-    //console.log(name)
-    let num = element.num;
-    let img = element.img;
-    let type = element.type;
-    let height = element.height;
-    let weight = element.weight;
-    let candy = element.candy;
-    let candyCount = element.candy_count;
-    let spawnChance = element.spawn_chance;
-    let avgSpawns = element.avg_spawns;
-    let spawnTime = element.spawn_time;
-    //console.log(weight);
-})
+document.getElementById('seeker').onchange= searchName;
+    let pokemonEncontrado = dataPokemon.filter(dataPokemon => {
+        dataPokemon.forEach(dataPokemon => {
+            let nombre = dataPokemon.name[0];
+            if(dataPokemon.name[0] == searchName){
+             pokemonEncontrado = searchName + [datapokemon.name];
+            }
+        })
+    }); 
+    return pokemonEncontrado;
+    id="name-pokemon"
+    document.getElementById(name-pokemon),innerHTML = dataPokemon.name;
 
 
-selector.addEventListener('change', () => {
-    let typePokemon = event.target.value;
-   // document.getElementById('listTypePokemon').innerHTML = window.pokemones.filterData( typePokemon, dataPokemon);
-   let valorPokemon = window.pokemones.filterData( typePokemon, dataPokemon);
-  // console.log(valorPokemon);
+    
+  
+    
+      
+ 
 
-  mostrarPokemon (valorPokemon)
-  return valorPokemon;
-})
+ 
 
-const mostrarPokemon = (valorPokemon)=> {
-    let mostrar=" ";
-    for (let key in valorPokemon)
-    {
-        mostrar = `<section id="listTypePokemon"><img src="${valorPokemon}" alt="" ></section>`
-    }
+// dataPokemon.forEach(element => {
+//     let name = element.name;
+//     let num = element.num;
+//     let img = element.img;
+//     let type = element.type;
+//     let height = element.height;
+//     let weight = element.weight;
+//     let candy = element.candy;
+//     let candyCount = element.candy_count;
+//     let spawnChance = element.spawn_chance;
+//     let avgSpawns = element.avg_spawns;
+//     let spawnTime = element.spawn_time;
+//     console.log(weight);
+// })
 
-    imprimir.insertAdjacentElement = mostrar;
-    return mostrar;
-}
+
+// selector.addEventListener('change', () => {
+ //  let typePokemon = event.target.value;
+//    document.getElementById('listTypePokemon').innerHTML = window.pokemones.filterData( typePokemon, dataPokemon);
+//    let valorPokemon = window.pokemones.filterData( typePokemon, dataPokemon);
+//   console.log(valorPokemon);
+
+//   mostrarPokemon (valorPokemon)
+//   return valorPokemon;
+// })
+
+// const mostrarPokemon = (valorPokemon)=> {
+//     let mostrar=" ";
+//     for (let key in valorPokemon)
+//     {
+//         mostrar = `<section id="listTypePokemon"><img src="${valorPokemon}" alt="" ></section>`
+//     }
+
+//     imprimir.insertAdjacentElement = mostrar;
+//     return mostrar;
+// }
