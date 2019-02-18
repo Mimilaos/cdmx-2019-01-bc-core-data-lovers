6d84d5001928873c5c5b27d3838b927eecf46815
giovanna08 <priscila_glz_27@hotmail.es>
2019-02-18 17:53:02 -0600

    Filtrado por nombre, por tipo. Responsive

--- src/data.js
+++ src/data.js
@@ -1,39 +1,29 @@
+opcionNombre = document.getElementById("listaNombres");
 //const clases = document.getElementById("class-selector");
 
 window.pokemones = {
 
  filterData: (typePokemon, dataPokemon)=> {
-   let listTypePokemon = [];
-   dataPokemon.forEach(element => {
-     let miType = element.type[0];
-     if(element.type[0] === typePokemon){
-       listTypePokemon = listTypePokemon + [element.name, element.num, element.img];
-     }
-   });
-   return listTypePokemon;
- },
+  let filtroPorTipo = dataPokemon.filter(element => element.type.includes(typePokemon));
+  return filtroPorTipo; 
+  },
 
  filterName: (nPokemon, dataPokemon)=> {
-   let pokemonEncontrado = [];
-   dataPokemon.forEach(pokemon =>{
-     let myPokemon = pokemon.name[0].toUpperCase();
-     if (myPokemon == nPokemon.toUpperCase()) {
-        console.log(pokemonEncontrado);
-      }
-    });
-    return pokemonEncontrado;
+ let pokemonEncontrado = dataPokemon.filter(dataPokemon => (dataPokemon.name.toUpperCase().match(nPokemon.toUpperCase()))); 
+  return (pokemonEncontrado)
   },
 }
 
-
   
-  
-//   scortData: ()=>{
-
-//   } 
-
+// sortName: (dataPokemon)=> {
+//   const filterName= dataPokemon.name.sort((a,b)=> {
+//       if(a.name > b.name){
+//         return 1;
+//       }
+//       if(a.name < b.name){
+//         return-1;
+//       }
+//     })
+//     return(filterName)
+//   },
 //   compuStats: ()=>{
-
-//   }
-// }
-
